19842f9a5cadcd7dba1c61a53311ddbf5b79172f
Anmol Tuteja <tutejaanmol54@gmail.com>
2025-09-08 16:25:14 +0530

    frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter,Route,Routes } from 'react-router';
+import Home from './components/Home/Home';
+import UserProvider from './components/UserProvider/UserProvider';
+
 function App() {
-  return (
-    <div></div>
-  )
+  return (<div>
+    <BrowserRouter>
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      </UserProvider>
+     </BrowserRouter>
+  </div>)
 }
 
-export default App
+export default App;
---
+++ client/src/components/UserProvider/UserProvider.jsx
@@ -0,0 +1,23 @@
+import { ClipLoader } from "react-spinners";
+import useUserStore from "../../store/userStore";
+
+function UserProvider({ children }) {
+  const { isLoading } = useUserStore();
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center bg-black text-white">
+        <ClipLoader
+          color="#36d7b7"
+          loading={true}
+          size={60}
+          aria-label="Loading Spinner"
+        />
+      </div>
+    );
+  }
+
+  return children;
+}
+
+export default UserProvider;
